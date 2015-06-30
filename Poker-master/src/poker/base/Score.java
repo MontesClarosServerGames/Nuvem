075b45d235c54c0b3620e550c7b6bf4d31c0a5c8
Rogério Lopes <unamed2015@gmail.com>
2015-06-30 20:39:36 -0300

    Revert "initial commit"
    This reverts commit 222dbc8062b4d4bf07917617b2e6a6b529e75ccc.

--- Poker-master/src/poker/base/Score.java
+++
@@ -1,9 +0,0 @@
-package poker.base;
-
-public class Score {
-	public Ranks evalRank(Card[] cards) {
-		//for the test class of score test
-		Hand hand = new Hand(false);
-		return hand.evalRank(cards);
-	}
-}
